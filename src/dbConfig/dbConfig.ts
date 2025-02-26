c211a3a33d58af1aa45d5400e8d02aa1c3d36aa2
Moksh Malde <maldemoksh437@gmail.com>
2025-02-26 22:44:41 +0530

    Remove user authentication routes and related components

--- src/dbConfig/dbConfig.ts
+++ src/dbConfig/dbConfig.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 export async function connect() {
     try {
-        mongoose.connect(process.env.MONGO_URI!);
+        mongoose.connect(process.env.MONGODB_URL!);
         const connection = mongoose.connection;
 
         connection.on('connected', () => {
